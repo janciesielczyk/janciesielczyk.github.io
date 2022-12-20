c5614d79fcde0d92c0d82e26767d999a625b4510
Jan <jan.ciesielczyk@grandparade.co.uk>
2022-12-20 11:05:11 +0100

    add counter and moving santa

--- december-21/game/src/game/Game.js
+++ december-21/game/src/game/Game.js
@@ -1,27 +1,53 @@
 import './Game.css';
-import santaGif from './../img/santa1-compressed.gif'
+import santaGif from './../img/santa-nobg.gif'
 import React, { useState } from "react";
 
 function Game() {
-
-  const [count, setCount] = useState(0);
-  window.ondragstart = function() {return false}
 
   const santaClicked = () => {
     setCount(count + 1);
   }
 
+  const directions = ['left', 'right']
+  const [newSantaId, setNewSantaId] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const getSantaElement = () => {
+    const direction = directions[getRandomInt(0,1)]
+    const style = {top: getRandomInt(20,50)+'%', left: getRandomInt(20,70)+'%'};
+    setNewSantaId(newSantaId + 1);
+    return (
+      <img key={() => newSantaId} onClick={()=>santaClicked} draggable="false" style={style} className={'santa santa-' + direction} alt="running santa" src={santaGif}></img>
+    )
+  }
+
+
+  const [santas, setSantas] = useState([
+    // ...new Array(5).fill(getSantaElement())
+    getSantaElement()
+  ]);
+
+
+
 
   return (
     <div className="game">
       <div className="game-view">
-        <img onClick={santaClicked} draggable="false" className="santa" alt="running santa" src={santaGif}></img>
+        {santas.map((santa) => (
+          santa
+        ))}
       </div>
       <div className="game-bar">
-        <div>{count}</div>
+        <div>Wynik: {count}</div>
       </div>
     </div>
   );
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export default Game;
