5642c571d887e62d5b4c3ba9970aa816736bde59
Jan <jan.ciesielczyk@grandparade.co.uk>
2022-12-21 02:31:59 +0100

    display more different santas

--- december-21/game/src/game/Game.js
+++ december-21/game/src/game/Game.js
@@ -1,40 +1,58 @@
 import './Game.css';
-import santaGif from './../img/santa-nobg.gif'
 import React, { useState } from "react";
+import MersenneTwister from 'mersenne-twister';
+
+const generator = new MersenneTwister();
 
 function Game() {
 
   const santaClicked = () => {
     setCount(count + 1);
+    setSantas([...santas, getSantaElement()]);
   }
 
-  const directions = ['left', 'right']
-  const [newSantaId, setNewSantaId] = useState(0);
+
+  // const directions = ['left', 'right', 'bottomright', 'bottomleft'];
+  const directions = ['left', 'right'];
+  const speeds = [1, 2, 3, 4]
+  const sizes = [1, 2, 3, 4]
   const [count, setCount] = useState(0);
 
   const getSantaElement = () => {
-    const direction = directions[getRandomInt(0,1)]
-    const style = {top: getRandomInt(20,50)+'%', left: getRandomInt(20,70)+'%'};
-    setNewSantaId(newSantaId + 1);
-    return (
-      <img key={() => newSantaId} onClick={()=>santaClicked} draggable="false" style={style} className={'santa santa-' + direction} alt="running santa" src={santaGif}></img>
-    )
+    const direction = directions[getRandomInt(0, directions.length - 1)]
+    const speed = speeds[getRandomInt(0, speeds.length - 1)]
+    const size = sizes[getRandomInt(0, sizes.length - 1)]
+    const style = {top: getRandomInt(0,80)+'%', left: getRandomInt(50, 150) + '%'};
+    // setNewSantaId(newSantaId + 1);
+    // console.log('getSantaElement', {newSantaId});
+    return (key, handler, arr) => {
+      const func = () => {
+        arr.splice(key, 1);
+        handler();
+      };
+      // return <img key={key} onClick={func} draggable="false" style={style} className={'santa santa-' + direction + ' santa-speed-' + speed } alt="running santa" src={santaGif}></img>
+      return <div key={key} onClick={func} style={style} className={'santa santa-' + direction + ' santa-speed-' + speed + ' santa-size-' + size }></div>
+    }
   }
 
 
   const [santas, setSantas] = useState([
-    // ...new Array(5).fill(getSantaElement())
-    getSantaElement()
+    ...new Array(5).fill({}).map(() => getSantaElement())
+    // getSantaElement()
   ]);
 
-
+  // useEffect(() => {
+  //   // console.log('useEffect');
+  //   // setSantas([...new Array(5).fill(getSantaElement(santaId))])
+  //   setSantas([getSantaElement(), getSantaElement()])
+  // }, [])
 
 
   return (
     <div className="game">
       <div className="game-view">
-        {santas.map((santa) => (
-          santa
+        {santas.map((santa, i) => (
+          santa(i, santaClicked, santas)
         ))}
       </div>
       <div className="game-bar">
@@ -47,7 +65,7 @@ function Game() {
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(generator.random() * (max - min + 1)) + min;
 }
 
 export default Game;
